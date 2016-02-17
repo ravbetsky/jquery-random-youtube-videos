538e24ee8796a6d1f1301bb1f64f40024f99ea58
ravbetsky <ravbetsky@gmail.com>
2016-02-17 12:35:52 +0300

    added main files

---
+++ main.js
@@ -0,0 +1,90 @@
+// Youtube videos
+var YoutubeRandomVideos = function(options) {
+    var self = this;
+
+    /* Callbacks */
+    self.callbackOnLoaded    = options.onLoaded;
+
+    /* Options */
+    self.channelId           = options.channelId;
+    self.countVideos         = options.count;
+    self.appKey              = options.key;
+    self.autostart           = options.autostart
+
+    /* Collection */
+    self.allVideosCollection = [];
+
+    /* call init */
+    self.init();
+};
+
+
+/* declare init */
+YoutubeRandomVideos.prototype.init = function() {
+  if(this.autostart) {
+    this.getChannelData();
+  }
+};
+
+/* all prototype methods below */
+/*-------------------------------------------------------------*/
+
+YoutubeRandomVideos.prototype.updateData = function(id, count) {
+  this.channelId = id;
+  this.countVideos = count;
+
+  this.getChannelData();
+};
+
+YoutubeRandomVideos.prototype.onLoaded = function(data) {
+  this.callbackOnLoaded(data);
+}
+
+YoutubeRandomVideos.prototype.getChannelData = function() {
+  var self = this;
+  $.get(
+     "https://www.googleapis.com/youtube/v3/channels",{
+     part : 'contentDetails',
+     id : self.channelId,
+     key: self.appKey },
+     function(data) {
+        $.each(data.items, function(i, item) {
+          self.allVideosCollection.push(item.contentDetails.relatedPlaylists.uploads);
+        });
+        $.each(self.allVideosCollection, function(i, item) {
+          self._getPlaylistVideos(item);
+        })
+    }
+  );
+};
+
+// Get videos from playlist
+YoutubeRandomVideos.prototype._getPlaylistVideos = function(playlistId) {
+  var self= this;
+  $.get(
+      "https://www.googleapis.com/youtube/v3/playlistItems",{
+      part : 'snippet',
+      maxResults: 35,
+      playlistId : playlistId,
+      key: self.appKey },
+      function(data) {
+          var sortedVideosCollection = self._randomizeArrayItems(data.items, self.countVideos);
+          self.onLoaded(sortedVideosCollection);
+      }
+  );
+}
+
+// Get random objects from an array
+YoutubeRandomVideos.prototype._randomizeArrayItems = function(data, count) {
+    var sortedArray = new Array(),
+        dataArray = data,
+        countToGet = count;
+
+    while(0 != countToGet) {
+      sortedArray.push(dataArray.splice(Math.floor(Math.random()*dataArray.length), 1)[0]);
+      countToGet--;
+    }
+    return sortedArray;
+}
+
+/*-------------------------------------------------------------*/
